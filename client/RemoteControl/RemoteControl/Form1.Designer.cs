754a7ecd8a9ba257196fc83e93bfee4410a2c25b
robcio89 <robcio89@gmail.com>
2016-03-12 19:01:50 +0100

    added RemoteControlServer

---
+++ client/RemoteControl/RemoteControl/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace RemoteControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtConsoleOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtConsoleOutput
+            // 
+            this.txtConsoleOutput.Location = new System.Drawing.Point(26, 165);
+            this.txtConsoleOutput.Multiline = true;
+            this.txtConsoleOutput.Name = "txtConsoleOutput";
+            this.txtConsoleOutput.Size = new System.Drawing.Size(435, 210);
+            this.txtConsoleOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 408);
+            this.Controls.Add(this.txtConsoleOutput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtConsoleOutput;
+    }
+}
+
---
+++ client/RemoteControl/RemoteControl/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteControl
+{
+	public partial class Form1 : Form
+	{
+		const string SERVER_IP = "192.168.1.4";
+		const Int32 PORT_NO = 12345;
+
+		IPAddress localAdd;
+		TcpListener server;
+		TcpClient client;
+
+		public Form1()
+		{
+			InitializeComponent();
+			localAdd = IPAddress.Parse(SERVER_IP);
+			server = new TcpListener(IPAddress.Any, PORT_NO);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			server.Start();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				String data = null;
+
+				while (true)
+				{
+					client = server.AcceptTcpClient();
+					txtConsoleOutput.AppendText("Połączono \n");
+
+					NetworkStream nwStream = client.GetStream();
+					byte[] buffer = new byte[client.ReceiveBufferSize];
+
+					int i;
+
+					// Loop to receive all the data sent by the client.
+					while ((i = nwStream.Read(buffer, 0, buffer.Length)) != 0)
+					{
+						// Translate data bytes to a ASCII string.
+						data = System.Text.Encoding.ASCII.GetString(buffer, 0, i);
+						txtConsoleOutput.AppendText("Otrzymano: " + data + "\n");
+
+						// Process the data sent by the client.
+						data = data.ToUpper();
+
+						byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+						// Send back a response.
+						nwStream.Write(msg, 0, msg.Length);
+						//txtConsoleOutput.AppendText("Sent: " + data + "\n");
+					}
+
+					client.Close();
+				}
+
+			}
+			catch (SocketException error)
+			{
+				txtConsoleOutput.AppendText("Wystąpił błąd " + error.Message + "\n");
+			}
+			finally
+			{
+				server.Stop();
+			}
+		}
+	}
+}
